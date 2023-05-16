0e12899e6b3162ead9b42010ca5d3cb45c08c046
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-16 18:24:06 +0800

    Added View Consultation Result Functionality

--- API/controllers/consultation_controller.js
+++ API/controllers/consultation_controller.js
@@ -67,7 +67,50 @@ const addConsultation = (req, res, next) => {
 
 }
 
+const viewConsultationResult = (req, res, next) => {
+    const consultationId = req.params.id;
+
+    if(!utils.checkMandatoryField(consultationId)){
+        res.status(404).json({
+            successful: false,
+            message: "Consultation id is missing."
+        });
+    }
+    else{
+        let consultationViewQuery = `SELECT u.name AS Psychologist, (SELECT name AS Patient FROM users u WHERE u.id = d.patient_id) AS Patient, i.name AS Illness, d.diagnosed_at AS 'Date and Time', d.note AS Note FROM users u
+        JOIN diagnoses d ON u.id = d.psychologist_id
+        JOIN illnesses i ON d.illness_id = i.id
+        WHERE d.id = ${consultationId}`;
+
+        database.db.query(consultationViewQuery, (viewErr, viewRows, viewResult) => {
+
+            if(viewErr){
+                res.status(500).json({
+                    successful: false,
+                    message: viewErr
+                });
+            }
+            else if(viewRows.length == 0){
+                res.status(400).json({
+                    successful: true,
+                    message: "Consultation id does not exist."
+                });
+            }
+            else{
+                res.status(200).json({
+                    successful: true,
+                    message: "Successfully got the consultation details",
+                    data: viewRows
+                });
+            }
+            
+        });
+    }
+
+
+}
 
 module.exports = {
-    addConsultation
+    addConsultation,
+    viewConsultationResult
 }
