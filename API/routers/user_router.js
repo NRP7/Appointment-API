1585985e546f0358d7dd9a30fd16d6611fa0b32f
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-16 14:36:09 +0800

    Added View All Users Router

--- API/routers/user_router.js
+++ API/routers/user_router.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user_controller');
 const userRouter = express.Router();
 
 userRouter.post('/add-user', userController.addUser);
+userRouter.get('/view-all', userController.viewAllUsers);
 
 module.exports = userRouter;
 
