9e6680c58deeecd0beb599792cf8674c13a851da
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-16 15:49:10 +0800

    Added View All Appointments Router

--- API/routers/appointment_router.js
+++ API/routers/appointment_router.js
@@ -4,5 +4,6 @@ const appointmentController = require('../controllers/appointment_controller');
 const appointmentRouter = express.Router();
 
 appointmentRouter.post('/book-appointment', appointmentController.bookAppointment);
+appointmentRouter.get('/view-all', appointmentController.viewAllAppointments);
 
 module.exports = appointmentRouter;
