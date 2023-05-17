9a1b2413a3e51ab81de84dd1bc9322a0e45fe15a
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-17 21:09:29 +0800

    Configured View Consultation Result Router

--- API/routers/consultation_router.js
+++ API/routers/consultation_router.js
@@ -4,6 +4,7 @@ const consultationController = require('../controllers/consultation_controller')
 const consultationRouter = express.Router();
 
 consultationRouter.post('/add-consultation', consultationController.addConsultation);
-consultationRouter.get('/view-consultation-result/:id', consultationController.viewConsultationResult);
+//consultationRouter.get('/view-consultation-result/:id', consultationController.viewConsultationResult);
+consultationRouter.get('/view-consultation-result/:psychologist_id/:patient_id', consultationController.viewConsultationResult);
 
 module.exports = consultationRouter;
