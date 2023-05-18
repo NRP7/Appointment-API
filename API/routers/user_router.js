092c111762634f3425eb73b3be8ec046a9265abb
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-18 16:45:21 +0800

    Renamed Variable name to username

--- API/routers/user_router.js
+++ API/routers/user_router.js
@@ -7,7 +7,7 @@ userRouter.post('/add-user', userController.addUser);
 userRouter.get('/view-all', userController.viewAllUsers);
 //userRouter.get('/view-user/:id', userController.viewUser);
 
-userRouter.get('/view-user/:name', userController.viewUser);
+userRouter.get('/view-user/:username', userController.viewUser);
 
 
 userRouter.put('/update-user-detail/:id', userController.updateUserDetail);
