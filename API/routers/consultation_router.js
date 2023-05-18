cbc84eaca9c08145ed54a4e2b894fe517dc1e9cf
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-18 17:44:36 +0800

    Added Commnets for HTTP Requests in Consultation Router)

--- API/routers/consultation_router.js
+++ API/routers/consultation_router.js
@@ -3,7 +3,10 @@ const consultationController = require('../controllers/consultation_controller')
 
 const consultationRouter = express.Router();
 
+// POST Method(s)
 consultationRouter.post('/add-consultation', consultationController.addConsultation);
+
+// GET Method(s)
 //consultationRouter.get('/view-consultation-result/:id', consultationController.viewConsultationResult);
 consultationRouter.get('/view-consultation-result/:psychologist_id/:patient_id', consultationController.viewConsultationResult);
 
