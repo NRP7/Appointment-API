8254984a7e0d384f009fb30094ed94f0adf4ec24
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-18 17:43:48 +0800

    Added Comments for HTTP Requests

--- API/routers/user_router.js
+++ API/routers/user_router.js
@@ -3,14 +3,19 @@ const userController = require('../controllers/user_controller');
 
 const userRouter = express.Router();
 
+// POST Method(s)
 userRouter.post('/add-user', userController.addUser);
-userRouter.get('/view-all', userController.viewAllUsers);
-//userRouter.get('/view-user/:id', userController.viewUser);
 
+// GET Method(s)
+//userRouter.get('/view-user/:id', userController.viewUser);
+userRouter.get('/view-all', userController.viewAllUsers);
 userRouter.get('/view-user/:username', userController.viewUser);
 
-
+// PUT Method(s)
 userRouter.put('/update-user-detail/:id', userController.updateUserDetail);
+
+// DELETE Method(s)
+userRouter.delete('/delete-user/:id', userController.deleteUser);
 
 module.exports = userRouter;
 
