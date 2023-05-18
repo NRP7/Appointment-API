7fcbf3c27cd25af4a658d2bf33e3ed5dd955c00a
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-18 17:45:14 +0800

    Added Commnets for HTTP Requests in Appointment Router)

--- API/routers/appointment_router.js
+++ API/routers/appointment_router.js
@@ -3,7 +3,10 @@ const appointmentController = require('../controllers/appointment_controller');
 
 const appointmentRouter = express.Router();
 
+// POST Method(s)
 appointmentRouter.post('/book-appointment', appointmentController.bookAppointment);
+
+// GET Method(s)
 appointmentRouter.get('/view-all', appointmentController.viewAllAppointments);
 
 module.exports = appointmentRouter;
