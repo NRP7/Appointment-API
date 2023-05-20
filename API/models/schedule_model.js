306decdcd2f887ac07a99c91f6d4b43f7fbb678b
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-20 21:40:53 +0800

    Added Status Id in Schedule Model

--- API/models/schedule_model.js
+++ API/models/schedule_model.js
@@ -1,9 +1,10 @@
-const schedule_model = (psychologist_id, patient_id, reserved_at) => {
+const schedule_model = (psychologist_id, patient_id, reserved_at, status_id) => {
 
     let Schedule = {
         psychologist_id: psychologist_id,
         patient_id: patient_id,
-        reserved_at: reserved_at
+        reserved_at: reserved_at,
+        status_id: status_id
     }
 
     return Schedule;
