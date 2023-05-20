8b290351d80ca4c5ff3078b662d579d63a5f700b
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-20 15:57:01 +0800

    Changed Username as Key to Id and Adjusted Function Spaces

--- API/controllers/user_controller.js
+++ API/controllers/user_controller.js
@@ -1,12 +1,14 @@
 const database = require('../models/connection_db');
 const userModel = require('../models/user_model');
 const utils = require('../../utils');
+const cryptoJS = require('crypto-js');
+
 
 const addUser = (req, res, next) => {
 
     let roleNum = req.body.role_num;
     let username =  req.body.username;
-    let userpass = req.body.userpass;
+    let password = req.body.password;
     let firstName = req.body.first_name;
     let lastName = req.body.last_name;
     let birthdate = req.body.birthdate;
@@ -16,41 +18,44 @@ const addUser = (req, res, next) => {
     let contactNumber = req.body.contact_number;
 
 
-    if(!utils.checkMandatoryFields([roleNum, username, userpass, firstName, lastName, birthdate, gender, address, email, contactNumber])) {
+    if (!utils.checkMandatoryFields([roleNum, username, password, firstName, lastName, birthdate, gender, address, email, contactNumber])) {
         res.status(404).json({
             successful: false,
-            message: "A user credential is not defined."
+            message: "A field is not defined."
         });
     }
     else {
         let userSelectQuery = `SELECT username FROM users WHERE username = '${username}'`;
 
         database.db.query(userSelectQuery, (selectErr,  selectRows, selectResult) => {
-            if(selectErr){
+            if (selectErr) {
                 res.status(500).json({
                     sucessful: false,
                     message: selectErr
                 });
             }
-            else{
-                if(selectRows.length > 0) {
+            else {
+                if (selectRows.length > 0) {
                     res.status(400).json({
                         sucessful: false,
-                        message: "User Name already exists."
+                        message: "User already exists."
                     });
                 }
-                else{
+                else {
+                    const userpass = cryptoJS.SHA1(password); // SHA1 Encryption is used.
+                    //const userpass = cryptoJS.AES.encrypt(password, key);
+
                     let userInsertQuery = `INSERT INTO users SET ?`;
                     let userObj = userModel.user_model(roleNum, username, userpass, firstName, lastName, birthdate, gender, address, email, contactNumber);
 
                     database.db.query(userInsertQuery, userObj, (insertErr, insertRows, insertResult) => {
-                        if(insertErr){
+                        if (insertErr) {
                             res.status(500).json({
                                 successful: false,
                                 message: insertErr
                             });
                         }
-                        else{
+                        else {
                             res.status(200).json({
                                 successful: true,
                                 message: "Successfully added new user."
@@ -65,28 +70,29 @@ const addUser = (req, res, next) => {
 
 }
 
+
 const viewAllUsers = (req, res, next) => {
-    let usersSelectQuery = `SELECT role_num AS Status, username AS Username, PASSWORD('userpass') AS Password, first_name AS 'First Name', last_name AS 'Last Name', DATE_FORMAT(birthdate, '%Y-%m-%d') AS Birthdate, gender AS Gender, address AS Address, email AS Email, contact_number AS 'Contact Number' FROM users`;
+    let usersSelectQuery = `SELECT id AS Id, role_num AS Status, username AS Username, userpass AS Password, first_name AS 'First Name', last_name AS 'Last Name', DATE_FORMAT(birthdate, '%Y-%m-%d') AS Birthdate, gender AS Gender, address AS Address, email AS Email, contact_number AS 'Contact Number' FROM users`;
     
     database.db.query(usersSelectQuery, (selectErr, selectRows, selectResult) => {
 
-        if(selectErr){
+        if (selectErr) {
             res.status(500).json({
                 successful: false,
                 message: selectErr
             });
         }
-        else if(selectRows.length == 0){
+        else if (selectRows.length == 0) {
             res.status(200).json({
                 successful: true,
                 message:"No users available in the database."
             });
         }
-        else{
+        else {
 
-            for(let i in selectRows){
+            for (let i in selectRows) {
 
-                if(selectRows[i].Status == 0){
+                if (selectRows[i].Status == 0) {
                     selectRows[i].Status = "Psychologist";
                 }
                 else {
@@ -103,45 +109,46 @@ const viewAllUsers = (req, res, next) => {
     });
 }
 
+
 const viewUser = (req, res, next) => {
-    //const userId = req.params.id;
+    let userId = req.params.id;
 
-    const username = req.params.username;
+    //const username = req.params.username;
 
-    if(!utils.checkMandatoryField(username)){
+    if (!utils.checkMandatoryField(userId)) {
         res.status(404).json({
             successful: false,
-            //message: "User id is missing."
-            message: "Username is missing."
+            //message: "User Name is missing."
+            message: "User id is missing."
         });
     }
     else {
-        // let userSelectQuery = `SELECT role_num AS Status, username AS Name, userpass AS Password, first_name AS 'First Name', last_name AS 'Last Name', birthdate AS 'Birth Date', gender AS Gender, address AS Address, email AS Email, contact_number AS 'Contact Number' FROM users u WHERE u.id = ${userId}`;
+        let userSelectQuery = `SELECT id AS Id, role_num AS Status, username AS Name, userpass AS Password, first_name AS 'First Name', last_name AS 'Last Name', DATE_FORMAT(birthdate, '%Y-%m-%d') AS 'Birth Date', gender AS Gender, address AS Address, email AS Email, contact_number AS 'Contact Number' FROM users u WHERE u.id = ${userId}`;
 
-        let userSelectQuery = `SELECT role_num AS Status, username AS Username, userpass AS Password, first_name AS 'First Name', last_name AS 'Last Name', DATE_FORMAT(birthdate, '%Y-%m-%d') AS Birthdate, gender AS Gender, address AS Address, email AS Email, contact_number AS 'Contact Number' 
-        FROM users u 
-        WHERE u.username LIKE '%${username}%'`;
+        // let userSelectQuery = `SELECT role_num AS Status, username AS Username, userpass AS Password, first_name AS 'First Name', last_name AS 'Last Name', DATE_FORMAT(birthdate, '%Y-%m-%d') AS Birthdate, gender AS Gender, address AS Address, email AS Email, contact_number AS 'Contact Number' 
+        // FROM users u 
+        // WHERE u.username LIKE '%${username}%'`;
     
         database.db.query(userSelectQuery, (selectErr, selectRows, selectResult) => {
 
-            if(selectErr){
+            if (selectErr) {
                 res.status(500).json({
                     successful: false,
                     message: selectErr
                 });
             }
-            else if(selectRows.length == 0){
+            else if (selectRows.length == 0) {
                 res.status(200).json({
                     successful: true,
-                    //message:"User id does not exist."
-                    message:"Username does not exist."
+                    //message:"User Name does not exist."
+                    message:"User Id does not exist."
                 });
             }
-            else{
+            else {
 
-                for(let i in selectRows){
+                for (let i in selectRows) {
 
-                    if(selectRows[i].Status == 0){
+                    if (selectRows[i].Status == 0) {
                         selectRows[i].Status = "Psychologist";
                     }
                     else {
@@ -159,12 +166,13 @@ const viewUser = (req, res, next) => {
     }
 }
 
-const updateUserDetail = (req, res, next) => {
 
-    const userUserName = req.params.username;
+const updateUserDetail = (req, res, next) => { // not done yet
+
+    let userId = req.params.id;
 
     let username = req.body.username;
-    let userpass = req.body.userpass;
+    let password = req.body.password;
     let firstName = req.body.first_name;
     let lastName = req.body.last_name;
     let birthdate = req.body.birthdate;
@@ -173,97 +181,94 @@ const updateUserDetail = (req, res, next) => {
     let email = req.body.email;
     let contactNumber = req.body.contact_number;
 
-    if(!utils.checkMandatoryFields([userUserName, username, userpass, firstName, lastName, birthdate, gender, address, email, contactNumber])){
+    if (!utils.checkMandatoryFields([userId, username, password, firstName, lastName, birthdate, gender, address, email, contactNumber])) {
         res.status(404).json({
             successful: false,
-            message: "A user credential is not defined."
+            message: "A field is not defined."
         });
         return;
     }
 
-    if(!utils.isString([username, userpass, firstName, lastName, birthdate, gender, address, email, contactNumber])){
+    if (!utils.isString([username, password, firstName, lastName, birthdate, gender, address, email, contactNumber])) {
         res.status(400).json({
             successful: false,
             message: "Incorrect user credential format."
         });
     }
-    else{
-        let userSelectQuery = `SELECT username FROM users WHERE username = '${userUserName}'`;
+    else {
+        let userSelectQuery = `SELECT id FROM users WHERE id = '${userId}'`;
         database.db.query(userSelectQuery, (selectErr, selectRows, selectResult) => {
-            if(selectErr){
+            if (selectErr) {
                 res.status(500).json({
                     successful: false,
                     message: selectErr
                 });
             }
-            else{
-                if(selectRows.length > 0){
+            else {
+                if (selectRows.length > 0) {
+
+                    const userpass = cryptoJS.SHA1(password);
                     //ALLOW THE UPDATING OF USER DETAILS
-                    let userUpdateQuery = `UPDATE users SET username = '${username}', userpass = '${userpass}', first_name = '${firstName}', last_name = '${lastName}', birthdate = '${birthdate}', gender = '${gender}', address = '${address}', email = '${email}', contact_number = '${contactNumber}' WHERE username = '${userUserName}'`;
+                    let userUpdateQuery = `UPDATE users SET username = '${username}', userpass = '${userpass}', first_name = '${firstName}', last_name = '${lastName}', birthdate = '${birthdate}', gender = '${gender}', address = '${address}', email = '${email}', contact_number = '${contactNumber}' WHERE id = '${userId}'`;
 
                     database.db.query(userUpdateQuery, (updateErr, updateRows, updateResult) => {
-                        if(updateErr){
+                        if (updateErr) {
                             res.status(500).json({
                                 successful: false,
                                 message: updateErr
                             });
                         }
-                        else{
+                        else {
                             res.status(200).json({
                                 successful: true,
                                 message: "Successfully updated user detail(s)."
                             });
-
                         }
-
                     });
-
                 }
-                else{
+                else {
                     res.status(400).json({
                         successful: false,
                         message: "User does not exist."
                     });
-
                 }
-
             }
         });
     }
 }
 
-const deleteUser = (req, res, next) => {
-    let userUserName = req.params.username;
 
-    if(!utils.checkMandatoryField(userUserName)){
+const deleteUser = (req, res, next) => {
+    const userId = req.params.id;
+
+    if (!utils.checkMandatoryField(userId)) {
         res.status(404).json({
             successful: false,
-            message: "Username is missing."
+            message: "User Id is missing."
         });
-
     }
-    else{
-        let userSelectQuery = `SELECT username FROM users WHERE username = '${userUserName}'`;
+    else {
+        let userSelectQuery = `SELECT id FROM users WHERE id = '${userId}'`;
 
         database.db.query(userSelectQuery, (selectErr, selectRows, selectResult) => {
-            if(selectErr){
+            if (selectErr) {
                 res.status(500).json({
                     successful: false,
                     message: selectErr
                 });
             }
-            else{
-                if(selectRows.length > 0){
-                    let userDeleteQuery = `DELETE FROM users WHERE username = '${userUserName}'`;
+            else {
+                if (selectRows.length > 0) {
+                    let userDeleteQuery = `DELETE FROM users WHERE id = '${userId}'`;
 
                     database.db.query(userDeleteQuery, (deleteErr, deleteRows, deleteResult) => {
-                        if(deleteErr){
+                        if (deleteErr) {
                             res.status(500).json({
                                 successful: false,
                                 message: deleteErr
                             });
                         }
-                        else{
+                        else {
                             res.status(200).json({
                                 successful: true,
                                 message: "Successfully deleted a user."
@@ -271,10 +276,10 @@ const deleteUser = (req, res, next) => {
                         }
                     });
                 }
-                else{
+                else {
                     res.status(400).json({
                         successful: false,
-                        message: "Username does not exist."
+                        message: "User Id does not exist."
                     });
                 }
             }
