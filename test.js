984d20b05224c76d28af7e85dcf10d3c3ed1a62e
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-20 14:40:15 +0800

    Test File

---
+++ test.js
@@ -0,0 +1,26 @@
+//const key = "javascript123456";
+
+// const test = () => {
+
+//     const key = "javascript123456";
+//     const plainText = "Hell0, world!";
+//     const password = "S1rD4ve"
+
+//     // const hashed = cryptoJS.SHA1(plainText);
+
+//     // console.log(hashed.toString());
+
+//     const encrypted = cryptoJS.AES.encrypt(plainText, key);
+
+//     console.log(`Encrypted: ${encrypted}`);
+//     console.log(`IV: ${encrypted.key}`);
+
+//     // const decrypted = cryptoJS.AES.decrypt(encrypted, key);
+
+//     // console.log(encrypted);
+
+//     // console.log(encrypted.ciphertext.toString());
+
+// }
+
+// test();
