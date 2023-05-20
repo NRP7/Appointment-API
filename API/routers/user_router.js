acb5eacefe5a8953b461a611264c18f3642166c5
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-20 15:57:50 +0800

    Reverted Path Variables

--- API/routers/user_router.js
+++ API/routers/user_router.js
@@ -9,13 +9,13 @@ userRouter.post('/add-user', userController.addUser);
 // GET Method(s)
 //userRouter.get('/view-user/:id', userController.viewUser);
 userRouter.get('/view-all', userController.viewAllUsers);
-userRouter.get('/view-user/:username', userController.viewUser);
+userRouter.get('/view-user/:id', userController.viewUser);
 
 // PUT Method(s)
-userRouter.put('/update-user-detail/:username', userController.updateUserDetail);
+userRouter.put('/update-user-detail/:id', userController.updateUserDetail);
 
 // DELETE Method(s)
-userRouter.delete('/delete-user/:username', userController.deleteUser);
+userRouter.delete('/delete-user/:id', userController.deleteUser);
 
 module.exports = userRouter;
 
