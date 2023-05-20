252358f7d7d14b16cabab7cee724c92fe833c6f4
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-20 21:40:15 +0800

    Added Schedule Model

---
+++ API/models/status_model.js
@@ -0,0 +1,14 @@
+const status_model = (name, description) => {
+
+    let Status = {
+        name: name,
+        description: description
+    }
+
+    return Status;
+
+}
+
+module.exports = {
+    status_model
+}
