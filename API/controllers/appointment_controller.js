821f307de0ea9246bee6a2e6b47ba294d48b4e2a
NRP7 <nathan.ronnie.paje@gmail.com>
2023-05-21 11:36:26 +0800

    Changed View All Appointments Function Query

--- API/controllers/appointment_controller.js
+++ API/controllers/appointment_controller.js
@@ -7,7 +7,6 @@ const bookAppointment = (req, res, next) => {
     let psychologistId = req.body.psychologist_id;
     let patientId = req.body.patient_id;
     let reservedAt = req.body.reserved_at;
-    let statusId = 2;
 
     if (!utils.checkMandatoryFields([psychologistId, patientId, reservedAt])) {
         res.status(404).json({
@@ -43,6 +42,8 @@ const bookAppointment = (req, res, next) => {
         //         });
         //     }
         //     else{
+                let statusId = 2;
+                
                 let scheduleSelectQuery = `SELECT psychologist_id, patient_id, reserved_at, status_id FROM schedules WHERE psychologist_id = ${psychologistId} AND patient_id = ${patientId} AND reserved_at = '${reservedAt}' AND status_id = ${statusId}`;
 
                 database.db.query(scheduleSelectQuery, (selectErr, selectRows, selectResult) => {
@@ -51,6 +52,7 @@ const bookAppointment = (req, res, next) => {
                             sucessful: false,
                             message: selectErr
                         });
+                        console.log(selectRows[0].status_id);
                     }
                     else{
                         if (selectRows.length > 0) {
@@ -60,6 +62,7 @@ const bookAppointment = (req, res, next) => {
                             });
                         }
                         else {
+
                             let scheduleInsertQuery = `INSERT INTO schedules SET ?`;
                             let scheduleObj = appointmentModel.schedule_model(psychologistId, patientId, reservedAt, statusId);
         
@@ -125,9 +128,9 @@ const updateAppointment = (req, res, next) => {
     let psychologistId = req.body.psychologist_id;
     let patientId = req.body.patient_id;
     let reservedAt = req.body.reserved_at;
-    let statusId = req.body.status_id;
+    //let statusId = req.body.status_id;
 
-    if (!utils.checkMandatoryFields([appointmentId, psychologistId, patientId, reservedAt, statusId])) {
+    if (!utils.checkMandatoryFields([appointmentId, psychologistId, patientId, reservedAt])) {
         res.status(404).json({
             successful: false,
             message: "An appointment credential is not defined."
@@ -151,9 +154,9 @@ const updateAppointment = (req, res, next) => {
     }
 
     else {
-        let appointmentIdSelectQuery = `SELECT id FROM schedules WHERE id = ${appointmentId}`;
+        let appointmentSelectAllQuery = `SELECT id, s.psychologist_id, s.patient_id, DATE_FORMAT(s.reserved_at, '%Y-%m-%d %k:%i:%s') AS reserved_at, s.status_id FROM schedules s WHERE s.id = ${appointmentId}`;
         
-        database.db.query(appointmentIdSelectQuery, (selectErr, selectRows, selectResult) => {
+        database.db.query(appointmentSelectAllQuery, (selectErr, selectRows, selectResult) => {
             if (selectErr) {
                 res.status(500).json({
                     successful: false,
@@ -163,40 +166,30 @@ const updateAppointment = (req, res, next) => {
             else {
                 if (selectRows.length > 0) {
 
-                    let appointmentSelectAllQuery = `SELECT s.psychologist_id, s.patient_id, DATE_FORMAT(s.reserved_at, '%Y-%m-%d %k:%i:%s') AS reserved_at, s.status_id FROM schedules s WHERE s.id = ${appointmentId}`;
+                    if (psychologistId == selectRows[0].psychologist_id && patientId == selectRows[0].patient_id && reservedAt == selectRows[0].reserved_at) {
+                        res.status(400).json({
+                            successful: false,
+                            message: "No changes were made, same schedule details are entered."
+                        }); 
+                    }
+                    else {
+                        let appointmentUpdateQuery = `UPDATE schedules SET psychologist_id = ${psychologistId}, patient_id = ${patientId}, reserved_at = '${reservedAt}' WHERE id = ${appointmentId}`;
 
-                    database.db.query(appointmentSelectAllQuery, (selErr, selRows, selResult) => {
-                        if (selErr) {
-                            res.status(500).json({
-                                successful: false,
-                                message: selErr
-                            }); 
-                        }
-                        else if (psychologistId == selRows[0].psychologist_id && patientId == selRows[0].patient_id && reservedAt == selRows[0].reserved_at && statusId == selRows[0].status_id) {
-                            res.status(400).json({
-                                successful: false,
-                                message: "No changes were made, same schedule details are entered."
-                            }); 
-                        }
-                        else {
-                            let appointmentUpdateQuery = `UPDATE schedules SET psychologist_id = ${psychologistId}, patient_id = ${patientId}, reserved_at = '${reservedAt}', status_id = ${statusId} WHERE id = ${appointmentId}`;
-
-                            database.db.query(appointmentUpdateQuery, (updateErr, updateRows, updateResult) => {
-                                if (updateErr) {
-                                    res.status(500).json({
-                                        successful: false,
-                                        message: updateErr
-                                    });
-                                }
-                                else {
-                                    res.status(200).json({
-                                        successful: true,
-                                        message: "Successfully updated schedule detail(s)."
-                                    });
-                                }
-                            });
-                        }
-                    });
+                        database.db.query(appointmentUpdateQuery, (updateErr, updateRows, updateResult) => {
+                            if (updateErr) {
+                                res.status(500).json({
+                                    successful: false,
+                                    message: updateErr
+                                });
+                            }
+                            else {
+                                res.status(200).json({
+                                    successful: true,
+                                    message: "Successfully updated schedule detail(s)."
+                                });
+                            }
+                        });
+                    }
                 } 
                 else {
                     res.status(400).json({
