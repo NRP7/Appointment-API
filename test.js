50f519c0ef5e4ee10dda31407d0f328bda42967b
NRP7 <nathan.ronnie.paje@gmail.com>
2023-06-08 21:54:55 +0800

    Added Test Function for Date and Time

--- test.js
+++ test.js
@@ -38,26 +38,60 @@
 
 // test2();
 
-const dateTest = () => {
+// const dateTest = () => {
 
-    // let date = new Date();
+//     // let date = new Date();
 
-    // console.log(date);
-    // console.log(date.getDay());
+//     // console.log(date);
+//     // console.log(date.getDay());
 
-    let date = "2023-06-04";
-    let time = "12:45"
+//     let date = "2023-06-04";
+//     let time = "12:45"
 
-    let dateTime = date + " " + time;
+//     let dateTime = date + " " + time;
 
-    console.log(dateTime);
-    console.log(`${date} ${time}`);
+//     console.log(dateTime);
+//     console.log(`${date} ${time}`);
 
-    let hour = "08";
-    console.log(hour);
+//     let hour = "08";
+//     console.log(hour);
 
-    console.log(time.length);
+//     console.log(time.length);
 
-}
+// }
 
-dateTest();
+// dateTest();
+
+// const testTimeStamp = () => {
+
+//     // function isAfter(birthdate) {
+        
+//     //     if(birthdate < )
+
+//     // }
+      
+//     let birthdate = "2023-06-07";
+
+//     let birthdateTimestamp = new Date(birthdate).getTime();
+
+//     const currentDate = new Date().getTime();
+//     const date = Date.now();
+
+//     console.log(`Current Date Timestamp: ${currentDate}`);
+//     console.log(`Current Date Timestamp: ${date}`);
+
+//     console.log(`Birthdate Timestamp: ${birthdateTimestamp}`);
+
+//     if ( birthdateTimestamp >= currentDate ) {
+//         console.log(`Birthdate IS beyond the current date`);
+//     }
+//     else {
+//         console.log(`Birthdate IS NOT beyond the current date`);
+//     }
+
+
+
+    
+// }
+
+// testTimeStamp();
